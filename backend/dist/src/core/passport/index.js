de7cd0d2d6b231ab9763dd61a2a232dae0f6bffe
Danilagreb <danilagreben2000@gmail.com>
2023-05-03 20:31:39 +0300

    fix

--- backend/dist/src/core/passport/index.js
+++ backend/dist/src/core/passport/index.js
@@ -5,19 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.applyStrategies = void 0;
 const user_repo_1 = require("../../user/user.repo");
-const github_1 = __importDefault(require("./github"));
-const google_1 = __importDefault(require("./google"));
 const jwt_1 = __importDefault(require("./jwt"));
 const local_1 = __importDefault(require("./local"));
-const slack_1 = __importDefault(require("./slack"));
-const vk_1 = __importDefault(require("./vk"));
 const yandex_1 = __importDefault(require("./yandex"));
 const applyStrategies = (passport) => {
     (0, yandex_1.default)(passport);
-    (0, slack_1.default)(passport);
-    (0, google_1.default)(passport);
-    (0, github_1.default)(passport);
-    (0, vk_1.default)(passport);
     (0, jwt_1.default)(passport);
     (0, local_1.default)(passport);
     passport.serializeUser(function (user, done) {
